skeptycal/ApplySyntax

commit bcb83d75b91e83ba9956ac74b9e21e5ce32476be

Fix project syntax not working
Fixes: #91
Ref: #94

ApplySyntax.py
@@ -323,6 +323,18 @@ class ApplySyntaxCommand(sublime_plugin.EventListener):
         active_settings = self.view.settings() if self.view else {}
         return active_settings.get(name, SETTINGS.get(name, default))
 
+    def get_project_setting(self, name, default=None):
+        """Get project settings."""
+
+        window = self.view.window()
+
+        if window is not None:
+            active_settings = window.project_data()
+            value = active_settings.get(name, default)
+        else:
+            value = default
+        return value
+
     def on_new(self, view):
         """Apply syntax on new file."""
         self.touch(view)
@@ -465,7 +477,7 @@ class ApplySyntaxCommand(sublime_plugin.EventListener):
         )
         # load any project-defined syntaxes
         project_syntaxes = self.create_extension_rule(
-            self.get_setting("project_syntaxes", [])
+            self.get_project_setting("project_syntaxes", [])
         )
 
         self.syntaxes = project_syntaxes + user_syntaxes + default_syntaxes

commit f67a5766a5bcf3b930c4481563d31fb6226a238c

Fix regression introduced in https://github.com/facelessuser/ApplySyntax/commit/bcb83d75b91e83ba9956ac74b9e21e5ce32476be
Fixes regression introduced in #93 and clarifies how to use project
specific rules which in turn actually answers #93.

Fixes #93

ApplySyntax.py
@@ -323,18 +323,6 @@ class ApplySyntaxCommand(sublime_plugin.EventListener):
         active_settings = self.view.settings() if self.view else {}
         return active_settings.get(name, SETTINGS.get(name, default))
 
-    def get_project_setting(self, name, default=None):
-        """Get project settings."""
-
-        window = self.view.window()
-
-        if window is not None:
-            active_settings = window.project_data()
-            value = active_settings.get(name, default)
-        else:
-            value = default
-        return value
-
     def on_new(self, view):
         """Apply syntax on new file."""
         self.touch(view)
@@ -477,7 +465,7 @@ class ApplySyntaxCommand(sublime_plugin.EventListener):
         )
         # load any project-defined syntaxes
         project_syntaxes = self.create_extension_rule(
-            self.get_project_setting("project_syntaxes", [])
+            self.get_setting("project_syntaxes", [])
         )
 
         self.syntaxes = project_syntaxes + user_syntaxes + default_syntaxes